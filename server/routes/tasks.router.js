0785122b3b0dd4a05f845f4fc1b646e43dd4e4e8
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-08-13 11:17:18 -0500

    debugging complete for GET test

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 // .. allows us to go up a directory 
-const pool = require('../modules/pool.js')
+const pool = require('../modules/pool.js');
 
 //GET
 router.get('/', (req, res) => {
     console.log('in GET /tasks');
-    // const queryText = 'SELECT * FROM "todo_list";';
-    // pool.query(queryText).then((result) => {
-    //     console.log('SELECT SUCESSFUL', result);
-    //     res.send(result.row);
-    // }).catch((error) => {
-    //     console.log('ERROR in GET /tasks', error);
-    //     res.sendStatus(500);
-    // });
+    const queryText = 'SELECT * FROM "todo_list";';
+    pool.query(queryText).then((result) => {
+        console.log('SELECT SUCESSFUL', result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('ERROR in GET /tasks', error);
+        res.sendStatus(500);
+    });
 })
 
 
--- server/server.js
+++ server/server.js
@@ -4,24 +4,14 @@ const express = require('express');
 const app = express();
 const PORT = 5007;
 // const port = process.env.PORT || 5007;
-
 // Express looks in server/public folder for anything we want to display
 app.use(express.static('server/public'));
 //body parser
 app.use(express.urlencoded({ extended: true }));
 
+//ROUTERS
 const router = require('./routes/tasks.router.js');
 app.use('/tasks', router);
-
-
-
-// test GET
-//  app.get('/tasks', (req, res) => {
-//     console.log('in GET /tasks')
-//     res.send(tasks);
-// })
-//ROUTERS
-
 
 
 //must be at bottom of server.js file
